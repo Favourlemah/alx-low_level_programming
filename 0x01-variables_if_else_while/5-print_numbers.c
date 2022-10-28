85debc66ae110a719b5bb3d563da73643fac67f5
Favourlemah <favourlemah@gmail.com>
2022-10-28 04:23:19 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all  base 10 digit,
+ *
+ * Return: 0 on SUCCESS
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0 ; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+
+	return (0);
+
+}
