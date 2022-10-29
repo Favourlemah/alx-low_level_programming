4de6648bbb9921fa7fb241b0ec5e9ea062f28980
Favourlemah <favourlemah@gmail.com>
2022-10-29 13:32:43 -0700

    task 0

---
+++ 0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - Program that prints "_putchar"
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int c;
+	char p[] = "_putchar";
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(p[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
