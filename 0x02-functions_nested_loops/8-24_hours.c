d733582a1afd975a1b49ae707c10817304d13c62
Favourlemah <favourlemah@gmail.com>
2022-10-30 12:59:13 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@
  *
  */
 
-void jack_bauer(void)
+void jack_bauer(void);
 
 {
 	int a, b, c, d;
@@ -19,7 +19,7 @@ void jack_bauer(void)
 			for (c = 48; c <= 53; c++)
 			{
 
-				for (d = 48; d <= 57; d)
+				for (d = 48; d <= 57; d++)
 				{
 					if (a >= 50 && b >= 52)
 						break;
