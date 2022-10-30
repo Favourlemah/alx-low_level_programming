18fe656e1921513915640eb4351a3110d273947d
Favourlemah <favourlemah@gmail.com>
2022-10-30 11:19:37 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+include "main.h"
+
+/**
+ *print_alphabet_x10- print x10 alphbet, In lowercase
+ *return: 0
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int i;
+	char ch;
+
+	for (i = 1; i < 11; i++)							{
+		for (ch = 97; ch < 123; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
+
+
